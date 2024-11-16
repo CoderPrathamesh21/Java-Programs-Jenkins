defc1833138cc2aeebe2177329e4fdba62a514cd
CoderPrathamesh21 <prathamdc99@gmail.com>
2024-11-16 20:06:50 +0530

    code push

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    public Student() {
+        System.out.println("Creating student instance");
+
+    }
+
+    public void sayHello() {
+        System.out.println("I'm a student");
+    }
+}
